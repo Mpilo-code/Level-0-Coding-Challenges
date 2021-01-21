9fe5d75f625210ee068e01269e77709a30a900fb
Mpilo-code <76687110+Mpilo-code@users.noreply.github.com>
2021-01-21 22:01:24 +0200

    Create Task 0.2.java

---
+++ Task 0.2.java
@@ -0,0 +1,18 @@
+public class MyClass {
+    public static void main(String args[]) {
+  
+     double x, y, z, a, b;
+     
+    x = 1 + 1 * 2;
+    y = (1 + 1) * 2;
+    z = 1 + ( 1 * 2 );
+    a = 1 + 1 * 2 / 2;
+    b = (double)(1 + (1 * 2) ) /  (double)2; 
+    
+    System.out.println("x: " + x);
+    System.out.println("y: " + y);
+    System.out.println("z: " + z);
+    System.out.println("a: " + a);
+    System.out.println("b: " + b);
+    }
+}
