9800808e3d5361c5296f4dfeaca791182299c4ac
Mpilo-code <76687110+Mpilo-code@users.noreply.github.com>
2021-01-22 14:24:59 +0200

    Created file and solved the task

---
+++ task-three.java
@@ -0,0 +1,16 @@
+
+public class Program {
+    
+    public static void main(String args[]) {
+        
+    hello("Mpilo");
+    
+    }
+    
+    public static void hello(String str) {
+        
+        System.out.println("Hello " + str);
+        
+    }
+    
+}
