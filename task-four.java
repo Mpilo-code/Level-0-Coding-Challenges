e2eb353748183a6118f4fda3f9047557cccbc1d8
Mpilo-code <76687110+Mpilo-code@users.noreply.github.com>
2021-01-22 14:37:16 +0200

    Created file and solved the task

---
+++ task-four.java
@@ -0,0 +1,16 @@
+public class Program {
+    
+    public static void main(String args[]) {
+        evenOrOdd(9);
+        
+    }
+      public static void evenOrOdd(double num) {
+          if(num % 2 == 0) {
+               System.out.println("even");
+          } else {
+               System.out.println("odd");   
+          }
+        
+    }
+    
+}
