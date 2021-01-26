cdf7ab905e441b081adca755621f3a161d963147
Mpilo-code <76687110+Mpilo-code@users.noreply.github.com>
2021-01-26 20:01:23 +0200

    Made the required changes
    Printed task 10 output as per instruction.

--- Task10.java
+++ Task10.java
@@ -1,25 +1,34 @@
+import java.util.*;
+
 public class Task10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		CommonCharacters("Out" , "Out");
+		ArrayList<String> commonChars = CommonCharacters("house" , "computers");
+		System.out.println(commonChars);
 		
 	}
 	
-	public static void CommonCharacters(String str1 , String str2) {
+	public static ArrayList<String> CommonCharacters(String str1 , String str2) {
 		
+	    ArrayList<String> commonChars = new ArrayList<String>(); 
 		
 		for(int i = 0 ; i < str1.length() ; i++) {
 			for(int k = 0 ; k < str2.length() ; k++) {
 				
 				if(Character.toString(str1.charAt(i)).equalsIgnoreCase(Character.toString(str2.charAt(k)))) {
-					System.out.println(Character.toString(str1.charAt(i)));
+				    commonChars.add(Character.toString(str1.charAt(i)));
+				
 				}
 				
 			}
 		}
 		
+		
+		
+	    return commonChars;
+		
 	}
 
-}
+}   
